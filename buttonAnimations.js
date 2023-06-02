d5c72d343d3c3ac431b27e97a71397874f79f599
Eduard <edi.raul@yahoo.com>
2023-06-02 10:33:01 +0200

    changes: - essay page is now somewhat functional; - trying to implement a thing where when an esssay group button is hovered it will display a snippet of the essay, and when clicked it will display the entire essay

---
+++ buttonAnimations.js
@@ -0,0 +1,9 @@
+// Get all the buttons inside the button container
+
+const buttons = document.querySelectorAll('.button');
+
+// Loop through each button and assign animation delays
+  buttons.forEach((button, index) => {
+  button.style.opacity = 0;
+  button.style.animationDelay = `${index * 0.2}s`;
+});
--- randomQuotes.js
+++ randomQuotes.js
@@ -8,6 +8,9 @@ var quotes = {
 
 var lastQuote = "";
 
+var isHovered = false;
+var isClicked = false;
+
 function getQuote(){
 	var authors = Object.keys(quotes);
 	var randomIdx = Math.floor(Math.random() * authors.length);
@@ -25,6 +28,9 @@ function getQuote(){
 }
 
 function writeQuote(){
+	if (isHovered == true || isClicked == true){
+		return;
+	}
 	var [quote, author] = getQuote();
 	
 	while (lastQuote === quote){
@@ -41,6 +47,7 @@ function writeQuote(){
 		document.getElementById("quote_text").innerHTML = `"${quote}" <br/> ${author}`;
 		container.style.opacity = 1;
 	}, 400);
+	
 
 	lastQuote = quote;
 }
@@ -60,8 +67,105 @@ function randomPosition(quote){
 
 function randomlyChangeQuote(){
 	writeQuote()
-
 	setInterval(writeQuote, 5000);
+}
+
+function hoverQuoteArea(group) {
+	var quoteArea = document.getElementById('quote_area');
+	
+	isHovered = true;
+	
+	getText(group);
+
+	quoteArea.classList.add('hovered');
+}
+
+function getText(group){
+	container = document.getElementById("quote_text");
+	container.style.transition = "ease 1s";
+	container.style.opacity = 0;
+	container.innerHTML.text = "";
+
+	try{
+		fetch(`essay_text/${group}.json`).then(response => response.json()).then(
+			data => {
+				title = data["title"]; 
+				text = data["text"];
+
+				document.getElementById("quote_text").style.top = "5px";
+				document.getElementById("quote_text").style.left = 0;
+				
+				var p_elem = document.createElement("p");
+				p_elem.id = "essay_text"
+				p_elem.style.top = "15px";
+				p_elem.style.left = 0;
+				p_elem.style.transition = "ease 1s";
+				p_elem.style.opacity = 0;
+				p_elem.style.color = "white";
+				p_elem.style.fontSize = "x-large";
+				p_elem.style.fontWeight = "450";
+				p_elem.style.textAlign = "center";
+				
+				var node = document.createTextNode(text);
+				p_elem.appendChild(node);
+				
+				document.getElementById("quote_area").appendChild(p_elem);
+
+				setTimeout(function() {
+					document.getElementById("quote_text").innerHTML = title;
+					p_elem.style.opacity = 1;	
+					container.style.opacity = 1;
+				}, 400);
+			}
+		);
+	} catch (error) {
+		console.error('Error:', error);
+	}
+}
+
+function resetQuoteArea() {
+	var quoteArea = document.getElementById('quote_area');
+	isHovered = false;
+	writeQuote();
+
+	var p_elem = document.getElementById("essay_text");
+	p_elem.style.opacity = 0;
+	// p_elem.style.position = "relative";
+	// p_elem.style.transition = "ease top 1s";
+	// p_elem.style.top = "100px";	
+	
+	setTimeout(function() {
+		quoteArea.removeChild(p_elem);
+	}, 400);
+
+
+	quoteArea.classList.remove('hovered');
+}
+
+function clickGroupButton(group) {
+	isClicked = true;
+
+	var quoteArea = document.getElementById('quote_area');
+	
+	quoteArea.classList.add("clicked");
+
+	getText(group);
+
+	var back_button = document.getElementById("goBack");
+	back_button.style.visibility = "visible";
+	back_button.style.opacity = 1;
+
+	quoteArea.classList.removed("hovered");
+}
+
+function goBackSelection() {
+	isClicked = false;
+
+	var back_button = document.getElementById("goBack");
+
+	quoteArea.classList.removed("clicked");
+	back_button.style.visibility = "hidden";
+	back_button.style.opacity = 0;
 }
 
 randomlyChangeQuote();
