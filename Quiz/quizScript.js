d894d5c312bd55b91f7583ebdef6cc4428c2ba11
Jonas Scholz <jojo.scholz@gmail.com>
2023-06-11 16:08:59 +0200

    Did some work on the Quiz

--- Quiz/quizScript.js
+++ Quiz/quizScript.js
@@ -2,23 +2,44 @@
 
 let question =
 {
-    questionLabel: "How are you?",
-    answers: ["Good!", "Great!", "Okay", "Not Great!"],
-    score: [[1,0,0,0], [0,1,0,0], [0,0,1,0], [0,0,0,1]]
+    questionLabel: "How much do you agree with the statement that 'Immigrants are often a burden on the economy'?",
+    answers: ["Strongly Agree", "Agree", "Disagree", "Strongly Disagree"],
+    score: [[2,0,2,2], [1,0,1,1], [0,1,0,0], [0,2,0,0]]
 };
 
 let question2 =
 {
-    questionLabel: "Who are you?",
-    answers: ["I don't know.", "Who are you?", "TEst", "TEst2"],
-    score: [[1,0,0,0], [0,1,0,0], [0,0,1,0], [0,0,0,1]]
+    questionLabel: "How important is the sovereignty of your state to you?",
+    answers: ["Very Important", "Important", "Unimportant", "Very unimportant"],
+    score: [[2,0,1,2], [1,0,1,1], [0,1,0,0], [0,2,0,0]]
+};
+
+let question3 =
+{
+    questionLabel: "How much do you agree with the statement that 'Leaving the NATO is a good idea'?",
+    answers: ["Strongly Agree", "Agree", "Disagree", "Strongly Disagree"],
+    score: [[2,0,1,0], [1,0,1,0], [0,1,0,1], [0,2,0,2]]
+};
+
+let question4 =
+{
+    questionLabel: "Do you agree that China is a big threat and that some actions should be taken?",
+    answers: ["Strongly Agree", "Agree", "Disagree", "Strongly Disagree"],
+    score: [[1,1,2,0], [1,1,1,0], [0,0,0,1], [0,0,0,2]]
+};
+
+let question5 =
+{
+    questionLabel: "Do you agree that Climate change is an important issue?",
+    answers: ["Strongly Agree", "Agree", "Disagree", "Strongly Disagree"],
+    score: [[1,2,0,0], [1,1,0,0], [0,0,1,1], [0,0,2,2]]
 };
 
 
 // ************************************************************************
 // Useful definitions
 
-let numButtons = 5;
+let numButtons = 6;
 
 function arrayMax(arr) {
   let len = arr.length;
@@ -35,19 +56,14 @@ function arrayMax(arr) {
 
 let questionContext = 
 {
-    questions: [question, question2],
+    questions: [question, question2, question3, question4, question5],
     currentQuestion: 0,
     scores: [0,0,0,0],
-    end: [["First option!", "First Text!"], ["Second option!", "Second Text!"], ["Third option!", "Third Text!"], ["Fourth option!", "Fourth Text!"]],
+    end: ["./quizEnd1.html", "./quizEnd2.html", "./quizEnd3.html", "./quizEnd4.html"],
     endFun: function()
     {
         maxIdx = arrayMax(this.scores);
-        document.getElementById("QuestionTitle").innerHTML = this.end[maxIdx][0];
-        document.getElementById("QuestionText").innerHTML = this.end[maxIdx][1];
-        for (let idx = 0; idx < numButtons; idx++)
-        {
-            document.getElementById("button" + idx).style.display = 'none'
-        }
+        window.open(this.end[maxIdx], "_self");
     }
 };
 
